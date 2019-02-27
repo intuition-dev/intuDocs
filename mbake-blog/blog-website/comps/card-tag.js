ce5163f00b404ccd71cf88629bd63b1b8b366899
Sandra Alvarez <samalvarez.dev@gmail.com>
2019-02-26 20:13:55 -0500

    setup card-tag file

--- mbake-blog/blog-website/comps/card-tag.js
+++ mbake-blog/blog-website/comps/card-tag.js
@@ -1,3 +1,25 @@
 
-riot.tag2('card-tag', '<div class="columns"> <div class="column col-6 col-sm-12"> <div class="card"> <div class="card-image"><img class="img-responsive" src="undefinedassets/img/blog/item-1.jpg"></div> <div class="card-top"> <button class="btn btn-primary">button</button> <h3>7 Must Have Tools For Web Designers</h3> <div>August 6, 2018 | Eldred Braun</div> </div> <div class="card-bottom"> <button class="btn btn-primary">Read More</button> </div> </div> </div> </div>', '', '', function(opts) {
+riot.tag2('card-tag', '<div class="columns"> <virtual each="{items}"> <div class="column col-6 col-sm-12"> <div class="card"> <div class="card-image"><img class="img-responsive" riot-src="{img_url}"></div> <div class="card-top"> <button class="btn btn-primary">button</button> <h3>7 Must Have Tools For Web Designers</h3> <div>August 6, 2018 | Eldred Braun</div> </div> <div class="card-bottom"> <button class="btn btn-primary">Read More</button> </div> </div> </div> </virtual> </div>', '', '', function(opts) {
+    console.info('oh hi tag');
+    this.on('*', function(evt) {
+        console.info('riot', evt);
+    });
+    this.items = [];
+    thiz = this;
+
+    this.render = function(data) {
+        console.info(data);
+        if(!data ) {
+            thiz.items = [];
+            thiz.update();
+            return;
+        }
+        console.info(Object.keys(data[0]));
+
+        let cloned = JSON.parse(JSON.stringify(data));
+        thiz.items = cloned;
+
+        thiz.update();
+
+    }.bind(this)
 });
