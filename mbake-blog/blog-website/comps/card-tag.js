b3d23c3a3d5af5a71a3974b7cfb73611f50f6579
Sandra Alvarez <samalvarez.dev@gmail.com>
2019-02-26 20:45:21 -0500

    add href link to card button

--- mbake-blog/blog-website/comps/card-tag.js
+++ mbake-blog/blog-website/comps/card-tag.js
@@ -1,5 +1,5 @@
 
-riot.tag2('card-tag', '<div class="columns"> <virtual each="{items}"> <div class="column col-6 col-sm-12"> <div class="card"> <div class="card-image"><img class="img-responsive" riot-src="{img_url}"></div> <div class="card-top"> <button class="btn btn-primary">button</button> <h3>{title}</h3> <div>{date} | Eldred Braun</div> </div> <div class="card-bottom"> <button class="btn btn-primary">Read More</button> </div> </div> </div> </virtual> </div>', '', '', function(opts) {
+riot.tag2('card-tag', '<div class="columns"> <virtual each="{items}"> <div class="column col-6 col-sm-12"> <div class="card"> <div class="card-image"><img class="img-responsive" riot-src="{img_url}"></div> <div class="card-top"> <button class="btn btn-primary">button</button> <h3>{title}</h3> <div>{date} | Eldred Braun</div> </div> <div class="card-bottom"><a href="{url}"> <button class="btn btn-primary">Read More</button></a></div> </div> </div> </virtual> </div>', '', '', function(opts) {
     console.info('oh hi tag');
     this.on('*', function(evt) {
         console.info('riot', evt);
@@ -18,6 +18,14 @@ riot.tag2('card-tag', '<div class="columns"> <virtual each="{items}"> <div class
 
         let cloned = JSON.parse(JSON.stringify(data));
         thiz.items = cloned;
+
+        let sz = thiz.items.length;
+        for(i = 0; i < sz; i++) {
+              var item = thiz.items[i];
+
+              item.url = '/posts/' + item.url;
+              console.info(item.url);
+          }
 
         thiz.update();
 
