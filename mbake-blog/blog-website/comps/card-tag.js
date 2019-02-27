6ea9505cf05122c25987248df223f8f36566ce94
Sandra Alvarez <samalvarez.dev@gmail.com>
2019-02-26 20:22:10 -0500

    fill data using items.json

--- mbake-blog/blog-website/comps/card-tag.js
+++ mbake-blog/blog-website/comps/card-tag.js
@@ -1,5 +1,5 @@
 
-riot.tag2('card-tag', '<div class="columns"> <virtual each="{items}"> <div class="column col-6 col-sm-12"> <div class="card"> <div class="card-image"><img class="img-responsive" riot-src="{img_url}"></div> <div class="card-top"> <button class="btn btn-primary">button</button> <h3>7 Must Have Tools For Web Designers</h3> <div>August 6, 2018 | Eldred Braun</div> </div> <div class="card-bottom"> <button class="btn btn-primary">Read More</button> </div> </div> </div> </virtual> </div>', '', '', function(opts) {
+riot.tag2('card-tag', '<div class="columns"> <virtual each="{items}"> <div class="column col-6 col-sm-12"> <div class="card"> <div class="card-image"><img class="img-responsive" riot-src="{img_url}"></div> <div class="card-top"> <button class="btn btn-primary">button</button> <h3>{title}</h3> <div>{date} | Eldred Braun</div> </div> <div class="card-bottom"> <button class="btn btn-primary">Read More</button> </div> </div> </div> </virtual> </div>', '', '', function(opts) {
     console.info('oh hi tag');
     this.on('*', function(evt) {
         console.info('riot', evt);
