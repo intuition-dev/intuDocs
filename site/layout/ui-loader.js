6d6e01a9186c8fde899a0dfcb77a5bb0038335c9
Natalia Melikyants <melikyants@gmail.com>
2019-08-07 20:51:11 +0200

    navbar responsive

--- site/layout/ui-loader.js
+++ site/layout/ui-loader.js
@@ -2,7 +2,7 @@
 depp.define({
    'custom-style': ['/../assets/css/custom.css']
 })
-depp.require(['FontsLoaded', 'bsDefaultStyle', 'DOM', 'font-awesome', 'custom-style'], function () {
+depp.require(['FontsLoaded', 'bsDefaultStyle', 'DOM', 'font-awesome', 'custom-style', 'bsjs'], function () {
    console.log('ready', Date.now() - _start)
 })
 
