e47c3ed451e401a3c315ed96ee8e2a67e99345a9
Natalia Melikyants <melikyants@gmail.com>
2019-08-07 20:45:47 +0200

    site

--- site/layout/ui-loader.js
+++ site/layout/ui-loader.js
@@ -1,6 +1,6 @@
 
 depp.define({
-   'custom-style': ['../assets/css/custom.css']
+   'custom-style': ['/../assets/css/custom.css']
 })
 depp.require(['FontsLoaded', 'bsDefaultStyle', 'DOM', 'font-awesome', 'custom-style'], function () {
    console.log('ready', Date.now() - _start)
