1f69730445f5c9989642103c9f2f9471be1a503b
vic <uptimevic@gmail.com>
2019-08-23 19:50:47 -0400

    ok

--- site/layouts/ui-loader.js
+++ site/layouts/ui-loader.js
@@ -2,9 +2,10 @@
 depp.define({
    'custom-style': ['/assets/css/custom.css']
 })
+
 depp.require(['FontsLoaded'], function () {
    console.log('ready:', Date.now() - _start)
-   depp.require([  'DOM', 'bsDefaultStyle', 'custom-style' ], function () {
+   depp.require([  'bsDefaultStyle', 'DOM', 'custom-style' ], function () {
       depp.done('commentoPre')
    })
 })
