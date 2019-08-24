b2bc7c621df6a9a63eea8227758a0d4eb8065b19
vic <uptimevic@gmail.com>
2019-08-23 21:09:57 -0400

    new

--- site/layouts/ui-loader.js
+++ site/layouts/ui-loader.js
@@ -7,6 +7,9 @@ depp.require(['FontsLoaded'], function () {
    console.log('ready:', Date.now() - _start)
    depp.require([  'bsDefaultStyle', 'DOM', 'custom-style' ], function () {
       depp.done('commentoPre')
+
+      depp.require('lazysizes')
+
    })
 })
 
