369f2b58bb2a51b7fe4b3420e486a1b1663587d4
vic <uptimevic@gmail.com>
2019-09-15 10:44:05 -0400

    ok

--- site/node-srv/index.ts
+++ site/node-srv/index.ts
@@ -5,18 +5,22 @@ const logger = require('tracer').console()
 // intu /////////////////////////////////////////
 
 const mainIApp = new ExpressRPC()
+mainIApp.makeInstance(['*'])
 
 async function app() {
    //www
-   mainIApp.serveStatic('..' + '/www')
+   mainIApp.serveStatic('..' + '/www', null, null)
 
    //catch all
    mainIApp.appInst.all('*', function (req, resp) {
+
       const path = req.path
       logger.trace('no route', path)
-      resp.json({'No route': path })
+      resp.redirect('/')
    })
 
    // start ////////////////////////////////////////////////////////
-   mainIApp.listen(8080)
+   mainIApp.listen(9081)
 }
+
+app()
