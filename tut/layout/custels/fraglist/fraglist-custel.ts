ec11e0da3358aa0df411b4203b9090d793a16873
vic <uptimevic@gmail.com>
2019-09-25 19:39:32 -0400

    ratio

---
+++ tut/layout/custels/fraglist/fraglist-custel.ts
@@ -0,0 +1,12 @@
+
+declare var jsyaml: any
+declare var $: any
+declare var Split: any
+declare var depp: any
+declare var disE: any
+
+
+depp.require(['jquery', 'pagination', 'mustache', 'js-yaml', 'poly'], function() {
+
+
+})
